29da21bb85a68c3b93df784c1e6d27a3aacf0cc8
Jésus Manoel de Paula Júnior <djeimeipeidju@gmail.com>
2025-09-06 08:21:36 -0300

    chore(firebase): adiciona config real do app Web

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,10 @@
-// firebase-config.js
-window.firebaseConfig = {
-  apiKey: "AIzaSyAmxo7T3jaXa6jXgHYjI0uPMwsr7iL06gE",
-  authDomain: "url-feminino.firebaseapp.com",
-  projectId: "url-feminino",
-  storageBucket: "url-feminino.appspot.com",
-  messagingSenderId: "1043621368070",
-  appId: "1:1043621368070:web:a18d09e904f5afb888ce87"
-};
+<script>
+  window.firebaseConfig = {
+    apiKey: "AIzaSyAmxo7T3jaXa6jXgHYjI0uPMwsr7iL06gE",
+    authDomain: "url-feminino.firebaseapp.com",
+    projectId: "url-feminino",
+    storageBucket: "url-feminino.firebasestorage.app",
+    messagingSenderId: "1043621368070",
+    appId: "1:1043621368070:web:a18d09e904f5afb888ce87"
+  };
+</script>
